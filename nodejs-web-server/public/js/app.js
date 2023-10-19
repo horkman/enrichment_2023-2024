7e58564a4ebb80e41ca3fb17d469e614f5da13d3
Adam Horky <adam.horky06@gmail.com>
2023-10-19 11:19:33 -0500

    updates

--- nodejs-web-server/public/js/app.js
+++ nodejs-web-server/public/js/app.js
@@ -9,25 +9,12 @@ function resetForm() {
 }
 
 function getRandomAnswer(question) {
-  // Our array/list of possible answers
-  // Remember: Array indexes always start at zero!
-  const possibleAnswers = [
-    "Yes.",
-    "No.",
-    "Reply hazy, try again.",
-    "Hmmm, maybe.",
-  ];
-
-  // First, let's get a random number between 0 and 1
-  const randomNumber = Math.random();
-
-  // Then, let's multiply the random number by the length of the answers array
-  // and then round the number down to get our randomized index
-  const randomIndex = Math.floor(randomNumber * possibleAnswers.length);
-
-  // Now we use the random array index to pull the value out of the
-  // array and return it
-  return possibleAnswers[randomIndex];
+  return fetch("/randomize", {
+    method: "POST", // Options are GET, POST, PUT, DELETE, PATCH
+    body: { question }, // A "body" can only be sent in POST, PUT, PATCH
+  })
+    .then((response) => response.json())
+    .catch((e) => console.log(e));
 }
 
 function displayAnswer(answer) {
@@ -50,11 +37,14 @@ $("#answerButton").click(function () {
   }
 
   // Now, we need to pass the question to our
-  // processing function so we can generate an answer
-  const answer = getRandomAnswer(question);
-
-  // Finally, we need to display the answer
-  displayAnswer(answer);
+  // processing function so we can generate an answer.
+  //
+  // getRandomAnswer is now a "Promise" frunction so we
+  // need to wait for the answer with a "then" function
+  getRandomAnswer(question).then(({ answer }) => {
+    // Finally, we need to display the answer
+    displayAnswer(answer);
+  });
 });
 
 $(".answer").hide();
--- nodejs-web-server/server/index.js
+++ nodejs-web-server/server/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // Initialize our web server
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+function getRandomAnswer(question) {
+  // Our array/list of possible answers
+  // Remember: Array indexes always start at zero!
+  const possibleAnswers = [
+    "Yes.",
+    "No.",
+    "Reply hazy, try again.",
+    "Hmmm, maybe.",
+  ];
+
+  // First, let's get a random number between 0 and 1
+  const randomNumber = Math.random();
+
+  // Then, let's multiply the random number by the length of the answers array
+  // and then round the number down to get our randomized index
+  const randomIndex = Math.floor(randomNumber * possibleAnswers.length);
+
+  // Now we use the random array index to pull the value out of the
+  // array and return it
+  return possibleAnswers[randomIndex];
+}
+
+app.post("/randomize", (req, res) => {
+  const request = req.body; // This will look like { question }
+  const question = request.question;
+  const answer = getRandomAnswer(question);
+  res.send({ answer });
 });
 
 // Always run this code last!
